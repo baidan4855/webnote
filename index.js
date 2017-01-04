57ef928185280fe64034e28fc917863e685324ef
蒋凯 <baidan4855@qq.com>
2017-01-04 19:23:21 +0800

    port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
-
+const port = process.env.PORT || 5000
 // response
 app.use(ctx => {
   ctx.body = 'Hello Koa';
   console.log('hello...')
 });
 
-app.listen(5000);
-console.log('app running at 5000')
+app.listen(port);
+console.log('app running at ' + port)
